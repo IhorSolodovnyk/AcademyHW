612ab155dfd2ebae7ed73e48998c48bbfbd3c1d6
IhorSolodovnyk <139598055+IhorSolodovnyk@users.noreply.github.com>
2023-10-22 17:30:17 +0300

    news_section

---
+++ second_project/js/dist/main.dev.js
@@ -0,0 +1,18 @@
+"use strict";
+
+$(document).ready(function () {
+  $('.article_slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    dots: true,
+    prevArrow: '.prev_arrow',
+    nextArrow: '.next_arrow',
+    responsive: [{
+      breakpoint: 968,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    }]
+  });
+});
